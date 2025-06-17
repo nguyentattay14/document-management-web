c45dcc4120b164c5eb71f1a9bc08b6df0846b7fd
Ydemo <yhoithiepuit@gmail.com>
2025-06-17 09:24:35 +0700

    Thêm job deploy cho CD trên self-hosted runner

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -11,6 +11,6 @@ public class HelloServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, World, I am a servlet, 17.06.2025!</h1>");
+        out.println("<h1>Hello, World, I am a servlet, thay đổi 17.06.2025!</h1>");
     }
 }
